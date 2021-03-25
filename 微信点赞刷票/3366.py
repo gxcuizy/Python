b96e0ec81191725b864e6f72538589fe4457afaa
gxcuizy <gxcuizy@163.com>
2021-03-25 16:25:52 +0800

    微信点赞刷票

---
+++ 微信点赞刷票/3366.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+利用代理IP刷点赞票（3366代理）
+author: gxcuizy
+date: 2021-03-25
+"""
+
+import requests
+import time
+from bs4 import BeautifulSoup
+
+
+class ThreeSix(object):
+    def __init__(self):
+        # 点赞接口地址
+        self.api_url = 'http://638140.szyuansl.com/topfirst.php?g=Wap&m=Vote&a=ticket'
+        # 点赞请求参数
+        self.post_param = {'zid': '11883', 'vid': '237', 'token': 'WMFAUktmdTwiHtTe'}
+        # 接口请求头信息
+        self.header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1320.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63010200)',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        # 代理IP地址
+        self.proxies = {}
+        # 超时时间
+        self.time_out = 20
+
+    def get_proxies_ip(self, ip_url):
+        """获取代理IP"""
+        ip_request = requests.get(url=ip_url)
+        html_content = ip_request.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+        # IP列表
+        tr_list = soup.select('table tbody tr')
+        ip_list = []
+        for tr in tr_list:
+            td_info = tr.select('td')
+            ip_host = td_info[0].text.strip()
+            ip_port = td_info[1].text.strip()
+            ip_base = '//' + ip_host + ':' + ip_port
+            ip_list.append(ip_base)
+        return ip_list
+
+    def print_msg(self, msg=''):
+        """打印信息"""
+        now_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        print('[' + now_time + '] ' + msg)
+
+    def run(self):
+        """执行程序"""
+        while True:
+            # 获取前11页
+            page_list = range(1, 11)
+            for page in page_list:
+                request_url = 'http://www.ip3366.net/?stype=1&page=' + str(page)
+                # 获取IP地址
+                ip_list = self.get_proxies_ip(request_url)
+                for ip_info in ip_list:
+                    self.proxies = {
+                        'http': 'http:' + ip_info,
+                        'https': 'https:' + ip_info
+                    }
+                    try:
+                        # 发送post请求
+                        request = requests.post(url=self.api_url, data=self.post_param, headers=self.header,
+                                                proxies=self.proxies, timeout=self.time_out)
+                        response_text = request.text
+                        self.print_msg(response_text)
+                    except Exception as err_info:
+                        # 异常信息
+                        self.print_msg(str(err_info))
+
+
+# 程序主入口
+if __name__ == "__main__":
+    obj = ThreeSix()
+    obj.run()
---
+++ 微信点赞刷票/refresh_times.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+刷新网页的浏览次数
+author: gxcuizy
+date: 2021-03-25
+"""
+
+import requests
+
+# 程序主入口
+if __name__ == "__main__":
+    # 活动网页地址
+    url = 'http://191456.szyuansl.com//topfirst.php?g=Wap&m=Vote&a=detail&token=WMFAUktmdTwiHtTe&id=237&zid=11883&href=1'
+    # 请求刷新次数
+    times = 500
+    # 请求头信息
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1320.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63010200)'
+    }
+    # 循环刷新
+    while times > 0:
+        requests.get(url=url, headers=header)
+        times -= 1
+        print('剩余刷新次数：' + str(times))
